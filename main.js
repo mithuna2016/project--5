80b130647ab939148e2ee0baee269833ca6cdd02
mithuna <ais_mini@yahoo.com>
2020-08-10 12:30:26 +0100

    try

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const express =require ("express");
 const bodyParser = require("body-parser");
-const { extend } = require("lodash");
- 
+const ejs = require("ejs");
+const _ = require("lodash");
+const http = require('http');
+
 const app = express();
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}))
-app.use('view engine' ,ejs);
 app.use(express.static("public"));
+
+
+ 
+app.get("/", function(req, res){
+    res.render("viewpage");
+  });
+  
+
+
+
+
+
+
+app.listen( process.env.PORT || 3000, function() {
+    console.log("Server started on port 3000");
+  });
+   
