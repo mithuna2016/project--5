25bf2186cedfbc45946116c500d93fb6942c3a5b
mithuna <ais_mini@yahoo.com>
2020-08-25 16:00:16 +0100

    creat viewpage

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const ejs = require("ejs");
+
+
 
 const cameraRoutes = require('./routes/camera');
 const teddyRoutes = require('./routes/teddy');
 const furnitureRoutes = require('./routes/furniture');
 
 const app = express();
+const http = require('http');
+
+
 
 mongoose.connect(
   'mongodb+srv://will:nAcmfCoHGDgzrCHG@cluster0-pme76.mongodb.net/test?retryWrites=true',
@@ -36,3 +42,59 @@ app.use('/api/teddies', teddyRoutes);
 app.use('/api/furniture', furnitureRoutes);
 
 module.exports = app;
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+
+
+//-------------------------------------------------------------------------------------------------------------
+//array for allitem
+let allItems =[{}];
+//get for view page
+app.get("/", function(req, res){
+  
+ const api ="http://localhost:3000/api/cameras";
+//view page http request
+   http.get(api,function(responce){
+    
+responce.on("data",function(data){
+const allProducts = JSON.parse(data);
+allItems = allProducts;
+
+ 
+
+});
+//connect the ejs
+  res.render("viewpage",{
+    allproducts:allItems
+  });
+ 
+});
+});
+
+// when click link to the about page
+app.get("/aboutpage/:itemId" ,function(req,res){
+ //route params
+  const requestedId = req.params.itemId;
+  
+ //access allItems array
+  allItems .forEach(function(ProductID){
+   
+   const storedId  = ProductID._id ;
+    
+    if (storedId === requestedId) {
+      res.render("aboutPage", {
+        imageUrl:ProductID.imageUrl,
+        name: ProductID.name,
+        price: ProductID.price,
+        description :ProductID.description,
+        lenses:ProductID.lenses
+      });
+    }
+    
+  });
+ 
+
+});
+
